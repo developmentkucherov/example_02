9a14370cd0b9e545abaf437674a154ee67b75cd5
Pavel <58953079+developmentkucherov@users.noreply.github.com>
2021-02-28 01:14:13 +0300

    v1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// burger
+$(document).ready(function () {
+  $('.slider').slick();
+  $('.header__burger').click(function (event) {
+    $('.header__burger, .header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
+
+// slider
+// $(document).ready(function () {
+//   $('.slider').slick();
+// });
